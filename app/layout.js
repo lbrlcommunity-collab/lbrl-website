732a35770e67a2f9b74de61d4f504b69d9b740df
lbrlcommunity-collab <lbrl.community@gmail.com>
2026-01-19 13:53:59 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'LBRL Tattoo Studio | Multi-Award Winning Custom Tattoos Vancouver WA',
   description: 'Multi-award winning custom tattoo studio in Vancouver, WA. Daniel Liberal brings 12+ years of experience in Japanese, blackwork, ornamental & floral designs. By appointment only.',
+  verification: {
+    google: 'ArYpD1e2h-wSSrfa7MVjVn13znFx-hoVl_DOl0pgQrc',
+  },
 }
 
 export default function RootLayout({ children }) {
